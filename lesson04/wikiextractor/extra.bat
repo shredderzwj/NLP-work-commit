092dfbf330c369f5f7886a712f1f686f9797a5eb
shredderzwj <shredderzwj@hotmail.com>
2019-08-07 12:51:19 +0800

    lesson04 commit

--- lesson04/wikiextractor/extra.bat
+++ lesson04/wikiextractor/extra.bat
@@ -1,3 +1,9 @@
-@echo off
+@echo OFF
 cd /d %~dp0
-python WikiExtractor.py --min_text_length 30 -o "D:\wiki" -b 20G "D:\wiki\zhwiki-20190720-pages-articles-multistream.xml.bz2"
+SET /p inpath="输入 wiki 语料库文件完整路径："
+SET /p outpath="输入输出文件夹路径："
+SET /p minlenth="输入最小文本长度："
+python WikiExtractor.py --min_text_length %minlenth% -o "%outpath%" -b 20G "%inpath%"
+ECHO.
+ECHO job done！
+PAUSE 
